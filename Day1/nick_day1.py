a8f281a53010545a4acd9a79baa902f8234e162d
Nick Andreasen <nickandreasen629@gmail.com>
2024-12-01 18:30:11 -0500

    add nick day 1 part 2

--- Day1/nick_day1.py
+++ Day1/nick_day1.py
@@ -6,3 +6,6 @@ data = df.values
 a = np.sort(data[:, 0])
 b = np.sort(data[:, 1])
 print(f"Answer: {np.sum(np.abs(a - b))}")
+
+similarityScore = sum([x*len(np.argwhere(x==b)) for x in a])
+print(f"Part Two Answer: {similarityScore}")
